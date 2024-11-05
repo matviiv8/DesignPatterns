d47cc9d2ba22bb52720e3f94351c1bd1c5198b9a
matviiv andrij <matviivandrij13@gmail.com>
2024-11-05 22:35:28 +0200

    feature: added Singleton pattern

---
+++ Singleton/Configuration.cs
@@ -0,0 +1,32 @@
+﻿namespace Singleton
+{
+    public class Configuration
+    {
+        private static Configuration _instance;
+
+        private Configuration()
+        {
+            SettingValue = "Default Setting";
+        }
+
+        public static Configuration Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Configuration();
+                }
+                return _instance;
+            }
+        }
+
+        public string SettingValue { get; set; }
+
+        public void DisplaySetting()
+        {
+            Console.WriteLine("Current Setting: " + SettingValue);
+        }
+    }
+
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using Singleton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Configuration firstConfig = Configuration.Instance;
+        firstConfig.SettingValue = "Custom Setting";
+        Configuration secondConfig = Configuration.Instance;
+
+        firstConfig.DisplaySetting();
+        secondConfig.DisplaySetting();
+
+        Console.WriteLine(ReferenceEquals(firstConfig, secondConfig));
+    }
+}
